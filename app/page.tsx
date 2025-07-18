c63fb113d467683954b4a5929e0ec07f13823b41
Amir-Webdev <fullstack.dev.amir@gmail.com>
2025-07-18 21:44:04 +0330

    Modified Global Layout And Added Stater Files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 function Home() {
   return (
     <>
-      <Navigation />
       <h1>پناهگاه بکر. مکانی برای آرامش شما</h1>
       <Link href="/cabins">مشاهده کابین ها</Link>
     </>
