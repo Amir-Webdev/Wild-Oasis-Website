ae697c46ddfc51535de03291b29f79e9104d7b47
Amir-Webdev <fullstack.dev.amir@gmail.com>
2025-07-24 09:01:24 +0330

    Modified Global Error Boundary

--- app/error.tsx
+++ app/error.tsx
@@ -1,18 +1,20 @@
 "use client";
 
-import { HiOutlineExclamationCircle } from "react-icons/hi2";
+type ErrorPropTypes = { error: Error; reset: () => void };
 
-function Error({ error }: { error: Error }) {
+function Error({ error, reset }: ErrorPropTypes) {
   return (
-    <div className="h-full grid items-center justify-center">
-      <h2 className="text-red-500 text-xl border border-red-500 rounded-xl flex items-center gap-6 py-6 px-10">
-        <HiOutlineExclamationCircle size={56} />
-        <span className="flex flex-col items-start gap-3">
-          <p>خطایی رخ داد. لطفا مجددا تلاش کنید.</p>
-          <p>پیام خطا: {error.message}</p>
-        </span>
-      </h2>
-    </div>
+    <main className="flex justify-center items-center flex-col gap-6">
+      <h1 className="text-3xl font-semibold">مشکلی پیش آمد.</h1>
+      <p className="text-lg">{error.message}</p>
+
+      <button
+        className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
+        onClick={reset}
+      >
+        تلاش مجدد
+      </button>
+    </main>
   );
 }
 
